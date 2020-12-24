1e6e54d99027cf3874185f49382ca1a96a055dd9
nwiizo <syu.m.5151@gmail.com>
2020-12-24 02:32:46 +0000

    [add] Dockek engine sdk

---
+++ docker/go/README.md
@@ -0,0 +1,1 @@
+https://docs.docker.com/engine/api/sdk/examples/ を読めば大体書いてる
---
+++ docker/go/ps/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	cli.NegotiateAPIVersion(ctx)
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Println("CONTAINER	IMAGE	State")
+		fmt.Println(container.ID[:12], container.Image, container.State)
+	}
+}
